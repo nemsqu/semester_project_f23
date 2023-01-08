e48d62b6729e673355658a0642cdeb58e6bff802
nemsqu <nelli.kemi@gmail.com>
2023-01-08 10:25:20 +0200

    fixes

--- models/Production.js
+++ models/Production.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-let emissionSchema = new Schema ({
+let productionSchema = new Schema ({
     "fuel": String,
     "emissions": Number,  //co2 or co2e ?
     "unit": String, //string or enum?
 });
 
 
-module.exports = mongoose.model("emission", emissionSchema);
+module.exports = mongoose.model("production", productionSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+const Electricity = require('../models/Electricity');
 var router = express.Router();
 const Emission = require('../models/Emission');
+const Production = require('../models/Production');
 
-//retrieve all data from database
+//retrieve all emission data from database
 router.get('/emissionData', function(req, res, next) {
   Emission.find({}, (err, emissions) => {
     if(err) {
@@ -16,7 +18,35 @@ router.get('/emissionData', function(req, res, next) {
   })
 });
 
-//for manually adding data to database, add parameters to body of the request
+//retrieve all fuel production data from database
+router.get('/productionData', function(req, res, next) {
+  Production.find({}, (err, production) => {
+    if(err) {
+      return res.status(404).send({'error': err});
+    }
+    if(production){
+      return res.send(production);
+    }else {
+      return res.json({error: "No emissions found"});
+    }
+  })
+});
+
+//retrieve all electicity production related data from database
+router.get('/electricityData', function(req, res, next) {
+  Electricity.find({}, (err, electricity) => {
+    if(err) {
+      return res.status(404).send({'error': err});
+    }
+    if(electricity){
+      return res.send(electricity);
+    }else {
+      return res.json({error: "No emissions found"});
+    }
+  })
+});
+
+//for manually adding emission data to database, add parameters to body of the request
 router.post('/emissionData', function(req, res, next) {
   new Emission({
     method: req.body.method,
@@ -24,6 +54,28 @@ router.post('/emissionData', function(req, res, next) {
     passengers: req.body.passengers,
     fuel: req.body.fuel,
     fuelConsumption: req.body.fuelConsumption
+  }).save((err) => {
+    if(err) return res.send(err);
+    return res.send("ok");
+  });
+});
+
+router.post('/productionData', function(req, res, next) {
+  new Production({
+    fuel: req.body.fuel,
+    emissions: req.body.emissions,
+    unit: req.body.unit
+  }).save((err) => {
+    if(err) return res.send(err);
+    return res.send("ok");
+  });
+});
+
+router.post('/electricityData', function(req, res, next) {
+  new Electricity({
+    fuel: req.body.fuel,
+    emissions: req.body.emissions,
+    unit: req.body.unit
   }).save((err) => {
     if(err) return res.send(err);
     return res.send("ok");
